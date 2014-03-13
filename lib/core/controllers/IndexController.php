0005012eb24af64a893f26925d67efb0e2ae08e0
Gabriell J <gabriell.juliana@blueacorn.com>
2014-03-13 12:53:35 -0400

    Created/Updated the Core_IndexController.php file, including adding functions to add/delete/reload as well as a validateIntent function.

--- lib/core/controllers/IndexController.php
+++ lib/core/controllers/IndexController.php
@@ -7,9 +7,78 @@
  */
 
 
+
+
+/*  Todo's:
+ *
+ *      _validateIntent
+ *          -   Need to pop an exception on false...
+ *
+ */
+
+
 class Core_IndexController {
 
     public function index(){
         die('Died in Core_IndexController::index()');
+    }
+
+
+    /**
+     * @param null $intent
+     */
+    protected function _add($intent = null)
+    {
+        $view = new Task_ViewController();
+
+        $intent = self::_validateIntent($intent);
+
+        $view->displayView($intent);
+    }
+
+
+    /**
+     * @param null $intent
+     */
+    protected function _delete($intent = null)
+    {
+        $view = new Task_ViewController();
+
+        $intent = self::_validateIntent($intent);
+
+        $view->displayView($intent);
+    }
+
+
+    /**
+     * @param null $intent
+     */
+    protected function _reload($intent = null)
+    {
+        $view = new Task_ViewController();
+
+        $intent = self::_validateIntent($intent);
+
+        $view->displayView($intent);
+    }
+
+
+    /**
+     * @description : This function will check if the $intent passed in is a valid non-null/non-numeric string.
+     *                  If true, return 'index', otherwise return $intent.
+     *
+     * @param null $intent
+     * @return string
+     */
+    private function _validateIntent($intent = null)
+    {
+//        if(is_null($intent) || is_numeric(substr($intent, 0, 1)))
+//        {
+//            //pop exception
+//            $intent = 'index';
+//        }
+
+
+        return (is_null($intent) || is_numeric((substr($intent, 0 , 1)))) ? 'index' : $intent;
     }
 } 
