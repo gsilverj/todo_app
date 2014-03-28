f2805d0906f031458d0ae2fcaf6ec9b2f4ffa021
Gabriell J <gabriell.juliana@blueacorn.com>
2014-03-28 13:20:15 -0400

    Altered the addTaskPanel to make the input textbox bigger and to include a button onto the end of the textbox, almost directly copy-pasta from get bootstrap websites input-group section.

--- themes/one_page_theme/panels/addTaskPanel.php
+++ themes/one_page_theme/panels/addTaskPanel.php
@@ -1,19 +1,30 @@
-<div class="container" id="addTaskPanel" >
+<!-- This piece is the area where user can add tasks to the todolist. -->
+<form role="form" action="<?php echo Core_XMLConfig::getBaseUrl() . 'task/add';?>" method="post">
+    <div class="form-group" id="addTaskFormGroupHeader">
+        <h2>ADD TASK</h2>
+    </div>
 
-    <form role="form" action="<?php echo Core_XMLConfig::getBaseUrl() . 'task/add';?>" method="post">
-        <div class="form-group">
-            <h2>ADD TASK</h2>
-        </div>
-        <div class="form-group">
-            <label for="taskDescriptionTbox">Task Description</label>
+    <div class="form-group" id="addTaskFormGroupBody">
+        <label for="taskDescriptionTbox">Task Description</label>
+
+        <div class="input-group input-group-lg" id="addTaskFormGroupInputGroup">
             <input type="text" class="form-control" id="taskDescriptionTbox" name="taskDescriptionTbox" placeholder="Please Enter A Task Description..." autofocus="autofocus"/>
+
+            <span class="input-group-addon">
+                <input type="submit" class="btn btn-success" value="ADD">
+            </span>
+        </div>
+
+        <div class="form-group" id="addTaskHelpBlockFormGroupBody">
             <p class="help-block">Please Enter A Task Description, Then Press The Submit Button.</p>
             <br />
-            <input type="submit" class="btn btn-success btn-lg" value="ADD">
         </div>
-    </form>
-</div>
+    </div>
+</form>
 
 
+<!-- older pieces used when initially created.-->
+<!-- original input text box       <input type="text" class="form-control" id="taskDescriptionTbox" name="taskDescriptionTbox" placeholder="Please Enter A Task Description..." autofocus="autofocus"/>-->
+<!-- original submit button  <input type="submit" class="btn btn-success btn-lg" value="ADD"> -->
 
 
