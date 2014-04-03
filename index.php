ce763ec09670d719881302cfe22064ebce5c4c4b
Gabriell J <gabriell.juliana@blueacorn.com>
2014-04-03 15:05:02 -0400

    Quick Save, Program is working.

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ ini_set('display_errors', 1);
 
 //includes, that could probably be put into bootstrap...
 
-
-
-
 include_once 'lib/core/Functions.php';
 include_once getcwd() . DS . 'lib' . DS . 'core' . DS . 'Bootstrap.php';
 include_once getcwd() . DS . 'lib' . DS . 'core' . DS . 'Autoloader.php';
